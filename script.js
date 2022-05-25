7a2738fba16551c82d0576e0bd939387e80da15d
Maia-th <thiagomaia633@unifei.edu.br>
2022-05-24 23:05:29 -0300

    Versão 1

---
+++ script.js
@@ -0,0 +1,72 @@
+function menuToggle() {
+  let menuArea = document.getElementById("menu-area");
+
+  if (menuArea.style.width == "50vw") {
+    menuArea.style.width = "0vw";
+  } else {
+    menuArea.style.width = "50vw";
+  }
+}
+
+function onlynumber(evt) {
+  var theEvent = evt || window.event;
+  var key = theEvent.keyCode || theEvent.which;
+  key = String.fromCharCode(key);
+  var regex = /^[0-9.-]+$/;
+  if (!regex.test(key)) {
+    theEvent.returnValue = false;
+    if (theEvent.preventDefault) theEvent.preventDefault();
+  }
+}
+
+function calc_raiz() {
+  let a = parseFloat(document.getElementById("a").value);
+  let b = parseFloat(document.getElementById("b").value);
+  let c = parseFloat(document.getElementById("c").value);
+  let raiz1, raiz2, r;
+
+  if (a == 0) {
+    alert("O termo A não pode ser 0!");
+    return;
+  } else if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    alert("Preencha todos os termos!");
+    return;
+  }
+
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d < 0) {
+    document.getElementById("hsol").innerText = "Delta Negativo";
+    document.getElementById("dnega").style.display = "flex";
+    document.getElementById("dzero").style.display = "none";
+    document.getElementById("dposi").style.display = "none";
+  } else if (d == 0) {
+    raiz1 = -b / (2 * a);
+    raiz1 = Math.round(raiz1 * 100) / 100;
+    document.getElementById("hsol").innerText = "Delta = 0 \n Raiz 1: " + raiz1;
+    document.getElementById("dnega").style.display = "none";
+    document.getElementById("dzero").style.display = "flex";
+    document.getElementById("dposi").style.display = "none";
+  } else {
+    r = Math.sqrt(d);
+    raiz1 = (-b + r) / (2 * a);
+    raiz2 = (-b - r) / (2 * a);
+    raiz1 = Math.round(raiz1 * 100) / 100;
+    raiz2 = Math.round(raiz2 * 100) / 100;
+    document.getElementById("hsol").innerText =
+      "Delta = " + d + "\n Raiz 1: " + raiz1 + "\n Raiz 2: " + raiz2;
+    document.getElementById("dnega").style.display = "none";
+    document.getElementById("dzero").style.display = "none";
+    document.getElementById("dposi").style.display = "flex";
+  }
+}
+
+function limp() {
+  document.getElementById("a").value = "";
+  document.getElementById("b").value = "";
+  document.getElementById("c").value = "";
+  document.getElementById("hsol").innerText = "Delta: \n Raiz 1: \n Raiz 2:";
+  document.getElementById("dnega").style.display = "none";
+  document.getElementById("dzero").style.display = "none";
+  document.getElementById("dposi").style.display = "none";
+}
